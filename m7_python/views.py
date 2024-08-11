57613aeed1b7043be7375cf493c88b98ab3b9a79
Jose Munoz <jose.e.munoz.nunez@gmail.com>
2024-08-11 13:39:40 -0400

    .-1 se crea carpeta templates con los archivos HTML( about, base, header, index , welcome) .-2 se agrega el logo y la carpeta static junto con las subcarpetas y archivos correspondientes .-3 carpeta registratio

--- m7_python/views.py
+++ m7_python/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'index.html', {})
+
+def acerca(request):
+    return render(request, 'acerca.html', {})
+
+def bienvenido(request):
+    return render(request, 'welcome.html', {})
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'm7_python',
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
